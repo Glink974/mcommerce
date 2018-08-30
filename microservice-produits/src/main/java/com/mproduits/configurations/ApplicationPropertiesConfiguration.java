0a68ad9f56cde1f795a6c60adfcb66f09ee4d6e8
Glink <gillesilotia@hotmail.fr>
2018-08-30 11:21:43 +0200

    mcommerce version 2

---
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -0,0 +1,20 @@
+package com.mproduits.configurations;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("mes-config")
+public class ApplicationPropertiesConfiguration {
+    private int limitDeProduits;
+
+    public int getLimitDeProduits(){
+        return limitDeProduits;
+    }
+
+    public void setLimitDeProduits(int limitDeProduits) {
+        this.limitDeProduits = limitDeProduits;
+    }
+
+}
