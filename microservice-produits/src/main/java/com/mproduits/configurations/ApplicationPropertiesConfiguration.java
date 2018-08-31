371e6ba44ce24f92f1254e10a7c603eefb3e064e
Glink <gillesilotia@hotmail.fr>
2018-08-31 08:20:22 +0200

    first commit

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("mes-config")
+@ConfigurationProperties("mes-configs")
 public class ApplicationPropertiesConfiguration {
     private int limitDeProduits;
 
